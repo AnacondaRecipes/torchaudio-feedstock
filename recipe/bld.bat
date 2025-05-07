a88dbda8f540add67386ed99c5523dc6eab788ca
Daniel Petry <dpetry@anaconda.com>
2025-05-07 15:16:52 -0500

    Set build number to zero, more cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,4 +6,4 @@ set USE_OPENMP=ON
 set BUILD_TORCHAUDIO_PYTHON_EXTENSION=ON
 set FFMPEG_ROOT=%LIBRARY_PREFIX%
 
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation 
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
