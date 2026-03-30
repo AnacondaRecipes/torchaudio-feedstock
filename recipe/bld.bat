7aa67755b3731986d30838d1ef55214aeaf19fd7
xkong-anaconda <xkong@anaconda.com>
2026-03-30 12:41:39 -0500

    Add torch lib path for Windows CUDA builds
    torch_python.lib is in site-packages/torch/lib/ which cmake can't
    find via CMAKE_FIND_ROOT_PATH alone. Add to LIB search path
    (same pattern as torchvision bld.bat).

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,4 +5,7 @@ set BUILD_SOX=OFF
 set USE_OPENMP=ON
 set BUILD_TORCHAUDIO_PYTHON_EXTENSION=ON
 
+:: Point the build system towards torch .lib files (needed for CUDA builds)
+set LIB=%PREFIX%\Lib\site-packages\torch\lib;%LIB%
+
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
