49e106b987b7b53de08fee53d7e3c7f70ad4ecfd
xkong-anaconda <xkong@anaconda.com>
2026-04-02 17:13:22 -0500

    Extend CUDA arch list with full Blackwell family (10.0;10.3;12.0;12.1+PTX)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,6 @@ set BUILD_TORCHAUDIO_PYTHON_EXTENSION=ON
 set LIB=%PREFIX%\Lib\site-packages\torch\lib;%LIB%
 
 :: CUDA 12.8: cap at 10.0 (pytorch 2.9.1 doesn't support arch 10.1)
-set TORCH_CUDA_ARCH_LIST=5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0+PTX
+set TORCH_CUDA_ARCH_LIST=5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0;10.3;12.0;12.1+PTX
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
