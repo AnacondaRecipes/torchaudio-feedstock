8889ac83b7f36c4d2aa937700ed0a26ab912118f
xkong-anaconda <xkong@anaconda.com>
2026-04-02 17:09:33 -0500

    Set TORCH_CUDA_ARCH_LIST on Windows, clean up build.sh for cuda128-only
    - bld.bat: set TORCH_CUDA_ARCH_LIST (nvcc 12.8 exports 10.1 which
      pytorch 2.9.1 doesn't support)
    - build.sh: simplify to single arch list (this PR is cuda128-only)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,4 +8,7 @@ set BUILD_TORCHAUDIO_PYTHON_EXTENSION=ON
 :: Point the build system towards torch .lib files (needed for CUDA builds)
 set LIB=%PREFIX%\Lib\site-packages\torch\lib;%LIB%
 
+:: CUDA 12.8: cap at 10.0 (pytorch 2.9.1 doesn't support arch 10.1)
+set TORCH_CUDA_ARCH_LIST=5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0+PTX
+
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
