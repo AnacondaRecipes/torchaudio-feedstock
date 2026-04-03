0af96104d68ef3c957031b5b5778c339faadc7cc
xkong-anaconda <xkong@anaconda.com>
2026-04-02 22:55:09 -0500

    Fix CUDA_HOME: use BUILD_PREFIX (nvcc in build env, not host)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,9 +8,9 @@ set BUILD_TORCHAUDIO_PYTHON_EXTENSION=ON
 :: Point the build system towards torch .lib files (needed for CUDA builds)
 set LIB=%PREFIX%\Lib\site-packages\torch\lib;%LIB%
 
-:: Point cmake to conda env's CUDA, not system CUDA (avoids header version mismatch)
-set CUDA_HOME=%LIBRARY_PREFIX%
-set CUDA_PATH=%LIBRARY_PREFIX%
+:: Point to build env's CUDA (nvcc is in build env, not host env)
+set CUDA_HOME=%BUILD_PREFIX%\Library
+set CUDA_PATH=%BUILD_PREFIX%\Library
 
 :: CUDA 13.x: dropped compute_50-61, min is 7.5 (Turing)
 set TORCH_CUDA_ARCH_LIST=7.5;8.0;8.6;8.9;9.0;10.0;10.3;12.0;12.1+PTX
