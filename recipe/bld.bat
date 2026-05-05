696b094cd06b5924c2b97e36eddadd6453c7f245
xkong-anaconda <xkong@anaconda.com>
2026-05-05 12:33:47 -0500

    Review fixes: fix version comment (2.9.1 → 2.10.0), remove py<314 skip (use PBP --skip-existing)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,7 +12,7 @@ set LIB=%PREFIX%\Lib\site-packages\torch\lib;%LIB%
 set CUDA_HOME=%BUILD_PREFIX%\Library
 set CUDA_PATH=%BUILD_PREFIX%\Library
 
-:: CUDA 12.8: cap at 10.0 (pytorch 2.9.1 doesn't support arch 10.1)
+:: CUDA 12.8: cap at 10.0 (pytorch 2.10.0 doesn't support arch 10.1)
 set TORCH_CUDA_ARCH_LIST=5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0+PTX
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
